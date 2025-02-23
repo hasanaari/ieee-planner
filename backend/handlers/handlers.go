a7308f4f1c5eed56fba93994a92b85f9015f614e
nicolenichole <lnicoleu23@gmail.com>
2025-02-23 14:14:57 -0600

    adding backend files

---
+++ backend/handlers/handlers.go
@@ -0,0 +1,35 @@
+package handlers
+import (
+	"encoding/json" // ecnode/deconde, sending/recieving JSON data
+	"fmt"
+	"net/http" // handling HTTP requests and responses
+)
+
+type Course struct {
+	Department string `json:"department"`
+	ID	int `json:"id"` // `: how filed is represented in JSON object
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Prerequisites []string `json:"prerequisites"`
+}
+
+// w: write response back to client
+// r: incoming HTTP request
+// http is from net/http package
+// func FunctionName(param1 Type1, param2 Type2)
+func CoursesHandler(w http.ResponseWriter, r *http.Request){
+	// initialise courses (array of Course structs)
+	courses := []Course{ //:= is variable declaration
+		{Department: "CS", ID: 111, Name:"Fundamentals of Computer Programming I", Description: "Fundamentals of CS", Prerequisites: []string{}},
+		{Department: "CS", ID: 150, Name:"Fundamentals of Computer Programming 1.5", Description: "Fundamentals of CS 2", Prerequisites: []string{"Fundamentals of Computer Programming I"}},
+	}
+	// Header: additional context about request/response
+	w.Header().Set("Content-Type", "application/json") // sets header of response to JSON format
+
+	encoder := json.NewEncoder(w) // JSON-encoded data, writes to w
+	encoder.Encode(courses) // convert courses into JSON, writes to w
+}
+
+func SchedulesHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This would return a schedule planner")
+}
